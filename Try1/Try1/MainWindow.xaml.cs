b7f83136813c2cfa5097856e618b0fab3408d38a
Mirzaf <mr.provorniy@mail.ru>
2018-12-01 22:00:30 +0200

    Buttons Update, Delete, fix Adding

--- Try1/Try1/MainWindow.xaml.cs
+++ Try1/Try1/MainWindow.xaml.cs
@@ -62,11 +62,13 @@ namespace Try1
                 command.Parameters.AddWithValue("Class", addBox3.Text);
                 command.Parameters.AddWithValue("Subclass", addBox4.Text);
                 command.Parameters.AddWithValue("Quantity", addBox5.Text);
+
                 command.Parameters.AddWithValue("Info", addBox6.Text);
 
                 //command.Parameters.AddWithValue("Name", addBox7.Text);
+                
                 string lines = addBox7.Text;
-                System.IO.File.WriteAllText(@addBox6.Text + ".txt", lines);
+                System.IO.File.WriteAllText(addBox6.Text + ".txt", lines, Encoding.UTF8);
 
 
                 await command.ExecuteNonQueryAsync();
@@ -88,14 +90,53 @@ namespace Try1
 
         }
 
-        private void BtnChange_Click(object sender, RoutedEventArgs e)
+        private async void BtnChange_Click(object sender, RoutedEventArgs e)
         {
+
+            try { 
+            if (lErr_.Visibility == Visibility.Visible)
+                lErr_.Visibility = Visibility.Hidden;
+
+            if (!string.IsNullOrEmpty(addBox1_.Text) && !string.IsNullOrWhiteSpace(addBox1_.Text) &&
+                !string.IsNullOrEmpty(addBox2_.Text) && !string.IsNullOrWhiteSpace(addBox2_.Text) &&
+                !string.IsNullOrEmpty(addBox5_.Text) && !string.IsNullOrWhiteSpace(addBox5_.Text))
+            {
+                SqlCommand command = new SqlCommand("UPDATE [Table] SET [Name]=@Name, [Price]=@Price, [Quantity]=@Quantity WHERE [Name]=N'" + comboMedName.SelectedItem.ToString() + "'", sqlConnection);
+
+                command.Parameters.AddWithValue("Name", addBox1_.Text);
+                command.Parameters.AddWithValue("Price", addBox2_.Text);
+                command.Parameters.AddWithValue("Quantity", addBox5_.Text);
+
+                
+                await command.ExecuteNonQueryAsync();
+                
+                addBox1_.Text = "";
+                addBox2_.Text = "";
+               
+                addBox5_.Text = "";
+                
+               
+            }
+            else
+            {
+                lErr_.Visibility = Visibility.Visible;
+
+                lErr_.Content = "Все поля должны быть заполнены!";
+            }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString(), ex.Source.ToString(), MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            comboMedName.SelectedItem = null;
+
+
 
         }
 
-        private void BtnSell_Click(object sender, RoutedEventArgs e)
+        private async void BtnSell_Click(object sender, RoutedEventArgs e)
         {
-
+                       
         }
 
         private async void BtnFind_Click(object sender, RoutedEventArgs e)
@@ -110,7 +151,7 @@ namespace Try1
 
                 while (await sqlReader.ReadAsync())
                 {
-                    string s = System.IO.File.ReadAllText(@Convert.ToString(sqlReader["Info"]) + ".txt", Encoding.Default).Replace("\n", " ");
+                    string s = System.IO.File.ReadAllText(Convert.ToString(sqlReader["Info"]) + ".txt", Encoding.Default).Replace("\n", " ");
 
                     textBlock1.Text = s;
                     textBlock2.Text = "Id: " + Convert.ToString(sqlReader["Id"]) + "\nЦена: " + Convert.ToString(sqlReader["Price"]) + " грн.\nКласс: " + Convert.ToString(sqlReader["Class"]) + "\nПодкласс: " + Convert.ToString(sqlReader["Subclass"]) + "\nКол-во на складе: " + Convert.ToString(sqlReader["Quantity"]);
@@ -143,6 +184,117 @@ namespace Try1
             addBox5.Text = "";
             addBox6.Text = "";
             addBox7.Text = "";
+
+            addBox1_.Text = "";
+            addBox2_.Text = "";
+            
+        }
+
+        private async void ComboMedName_Loaded(object sender, RoutedEventArgs e)
+        {
+            comboMedName.Items.Clear();
+           
+            SqlDataReader sqlReader = null;
+            SqlCommand command = new SqlCommand("SELECT [Name] FROM [Table]", sqlConnection);
+            try
+            {
+                
+                sqlReader = await command.ExecuteReaderAsync();
+
+                while (await sqlReader.ReadAsync())
+                {
+
+                    comboMedName.Items.Add(Convert.ToString(sqlReader["Name"]));
+                }
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show(ex.Message.ToString(), ex.Source.ToString(), MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            finally
+            {
+                if (sqlReader != null)
+                    sqlReader.Close();
+            }
+
+        }
+
+        private async void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            SqlDataReader sqlReader = null;
+            try
+            {
+                SqlCommand command = new SqlCommand("SELECT * FROM [Table] WHERE [Name] LIKE N'" + comboMedName.SelectedItem.ToString() + "'", sqlConnection);
+
+                sqlReader = await command.ExecuteReaderAsync();
+
+                while (await sqlReader.ReadAsync())
+                {
+                    //string s = Convert.ToString(System.IO.File.ReadAllText(Convert.ToString(sqlReader["Info"]) + ".txt", Encoding.Default).Replace("\n", " "));
+
+                    
+                    addBox1_.Text = Convert.ToString(sqlReader["Name"]);
+                    addBox2_.Text = Convert.ToString(sqlReader["Price"]);
+                   
+                    addBox5_.Text = Convert.ToString(sqlReader["Quantity"]);
+
+                }
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show(ex.Message.ToString(), ex.Source.ToString(), MessageBoxButton.OK, MessageBoxImage.Error); Мне похуй!
+            }
+            finally
+            {
+                if (sqlReader != null)
+                    sqlReader.Close();
+            }
+             
+        }
+
+        
+
+        private async void ComboDel_Loaded(object sender, RoutedEventArgs e)
+        {
+            comboDel.Items.Clear();
+
+            SqlDataReader sqlReader = null;
+
+            try
+            {
+                SqlCommand command = new SqlCommand("SELECT [Name] FROM [Table]", sqlConnection);
+                sqlReader = await command.ExecuteReaderAsync();
+
+                while (await sqlReader.ReadAsync())
+                {
+
+                    comboDel.Items.Add(Convert.ToString(sqlReader["Name"]));
+                }
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show(ex.Message.ToString(), ex.Source.ToString(), MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            finally
+            {
+                if (sqlReader != null)
+                    sqlReader.Close();
+            }
+        }
+
+        private async void BtnDel_Click(object sender, RoutedEventArgs e)
+        {
+            
+
+            SqlCommand command = new SqlCommand("DELETE FROM [Table] WHERE [Name]= @Name", sqlConnection);
+            command.Parameters.AddWithValue("Name", comboDel.SelectedItem.ToString());
+            await command.ExecuteNonQueryAsync();
+            comboDel.SelectedItem = null;
+            //}
+            /*catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString(), ex.Source.ToString(), MessageBoxButton.OK, MessageBoxImage.Error);
+            }*/
         }
     }
 }
