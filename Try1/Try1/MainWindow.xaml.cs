57e21b59be6b9a3af588b8af98bb09d067bacfbd
Bened1ctoos <boevoy1kusia@gmail.com>
2018-12-08 12:59:09 +0200

    adding ATX tree

--- Try1/Try1/MainWindow.xaml.cs
+++ Try1/Try1/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Data.SqlClient;
 using System.Data;
+using Path = System.IO.Path;
 
 namespace Try1
 {
@@ -24,20 +25,20 @@ namespace Try1
     {
         SqlConnection sqlConnection;
 
+        
+        
         public MainWindow()
         {
             InitializeComponent();
         }
-
+        
         private async void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            string connectionString = @"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename = D:\Archive\Project\Могилянка\С#\git\-medicine\Try1\Try1\Database1.mdf;Integrated Security=True";
+            string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\Программы\Git\-medicine\Try1\Try1\Database1.mdf;Integrated Security=True";
 
             sqlConnection = new SqlConnection(connectionString);
 
             await sqlConnection.OpenAsync();
-
-            
 
 
         }
@@ -144,5 +145,119 @@ namespace Try1
             addBox6.Text = "";
             addBox7.Text = "";
         }
+
+
+        private class Item
+        {
+            public string Class { get; set; }
+            public string Subclass { get; set; }
+            public string Name { get; set; }
+            public string Info { get; set; }
+        }
+
+        private async void TreeView_Loaded(object sender, RoutedEventArgs e)
+        {
+            string connectionString =
+                   string.Format(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename={0};Integrated Security=True",
+                       Path.GetFullPath("Database1.mdf"));
+
+            sqlConnection = new SqlConnection(connectionString);
+
+            await sqlConnection.OpenAsync();
+
+            SqlDataReader sqlReader = null;
+            //SqlCommand command = new SqlCommand("SELECT * FROM [Table] WHERE Name LIKE "+@findBox.Text.ToString(), sqlConnection);
+            SqlCommand command = new SqlCommand("SELECT * FROM [Table]", sqlConnection);
+
+            try
+            {
+                sqlReader = await command.ExecuteReaderAsync();
+
+                var items = new List<Item>();
+
+                while (await sqlReader.ReadAsync())
+                {
+                    /*TreeViewItem item = new TreeViewItem();
+                    
+
+                    item.Header = Convert.ToString(sqlReader["Class"]);
+                    item.ItemsSource = new string[] { Convert.ToString(sqlReader["Subclass"]), Convert.ToString(sqlReader["Name"]) };*/
+
+                    items.Add(new Item
+                    {
+                        Class = Convert.ToString(sqlReader["Class"]).Trim(),
+                        Subclass = Convert.ToString(sqlReader["Subclass"]),
+                        Name = Convert.ToString(sqlReader["Name"]),
+                        Info = Convert.ToString(sqlReader["Info"])
+                    });
+
+
+
+
+
+                    /*// ... Get TreeView reference and add both items.
+                        var tree = sender as TreeView;
+
+                        tree.Items.Add(item);
+                        */
+                }
+
+
+
+                foreach (var item in items.GroupBy(x => x.Class))
+                {
+                    var treeViewItem = new TreeViewItem { Header = item.Key };
+                    foreach (var subItem in item.GroupBy(x => x.Subclass))
+                    {
+                        var subclassTreeViewItem = new TreeViewItem
+                        {
+                            Header = subItem.Key,
+                            ItemsSource = subItem.Select(x => x.Name)
+
+                        };
+
+                        treeViewItem.Items.Add(subclassTreeViewItem);
+                    }
+
+                    tree.Items.Add(treeViewItem);
+                }
+
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString(), ex.Source.ToString(), MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            finally
+            {
+                if (sqlReader != null)
+                    sqlReader.Close();
+            }
+
+        }
+        private void TreeView_SelectedItemChanged(object sender,
+           RoutedPropertyChangedEventArgs<object> e)
+        {
+            var tree = sender as TreeView;
+
+            // ... Determine type of SelectedItem.
+            if (tree.SelectedItem is TreeViewItem)
+            {
+                // ... Handle a TreeViewItem.
+                var item = tree.SelectedItem as TreeViewItem;
+                this.Title = "Selected header: " + item.Header.ToString();
+            }
+            else if (tree.SelectedItem is string)
+            {
+                // ... Handle a string.
+                this.Title = "Selected: " + tree.SelectedItem.ToString();
+            }
+        }
+
     }
-}
+
+    }
+
+    
+
